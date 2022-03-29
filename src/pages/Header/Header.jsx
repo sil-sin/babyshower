e8145c94d76c9a655fef358ada055f351914b163
Silvi Sinanaj <silviberat@gmail.com>
2022-03-29 20:51:40 +0200

    testing brazil

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -5,31 +5,8 @@ export default function Header(props) {
   return (
     <div className="header">
       <div>
-        <p>
-          Baby Liam is coming, and we will be very happy to celebrate this
-          moment with our friends at his baby shower.
-          <br />
-          <br />
-          April 9th
-          <br />
-          3 pm - 5 pm
-          <br />
-          Urbanstr. 6 - courtyard (to be confirmed)
-          <br />
-          <br />
-          We hope to see you there!
-          <br />
-          <br />
-          Gi and Sil
-          <br />
-          <br />
-          <br />
-          Please RSVP below.
-          <br />
-          Baby gift registry available at the <b>Gifts</b> tab above.
-          <br />
-          Easy and quick way to do a Schnelltest before you come: Testzentrum Urbanstr. 1
-        </p>
+       
+      OLA!
       </div>
 
       <div>
