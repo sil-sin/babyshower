be3b1b6e347c06172d583db46639fe6702c4dd4f
Silvi Sinanaj <silviberat@gmail.com>
2022-04-02 12:26:05 +0200

    remove error at taken

--- src/pages/Gift/Gift.jsx
+++ src/pages/Gift/Gift.jsx
@@ -40,7 +40,7 @@ export default function Gift() {
             });
           })
           .catch((err) => {
-            next(err);
+            throw(err);
             console.log(err);
           });
       } else {
@@ -56,7 +56,7 @@ export default function Gift() {
             });
           })
           .catch((err) => {
-            next(err);
+            throw(err);
             console.log(err);
           });
       }
