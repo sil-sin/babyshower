83c0106d2cf0389ef1e8ad15c3493beb49512e28
Silvi Sinanaj <silviberat@gmail.com>
2022-05-01 16:54:18 +0200

    height of list fix

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -7,28 +7,16 @@ export default function Header(props) {
       <div>
         <p>
           Baby Liam is coming, and we will be very happy to celebrate this
-          moment with our friends at his baby shower.
-          <br />
-          <br />
-          April 9th
-          <br />
-          3 pm - 5 pm
-          <br />
-          Urbanstr. 6 - courtyard (to be confirmed)
-          <br />
-          <br />
-          We hope to see you there!
+          moment with our friends.
           <br />
           <br />
           Gi and Sil
           <br />
           <br />
           <br />
-          Please RSVP below.
-          <br />
           Baby gift registry available at the <b>Gifts</b> tab above.
           <br />
-          Easy and quick way to do a Schnelltest before you come: Testzentrum Urbanstr. 1
+         Update on Birth coming soon ...
         </p>
       </div>
 
